9fdfd23e50bad59b4126bc5a98d9ab886c828a55
loaywaleed <loaywaleed9@gmail.com>
2024-03-14 20:52:19 +0200

    iterables

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,8 +2,9 @@
 """
 Script that uses duck typing
 """
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> Tuple[str, int]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Using iterables and sequence"""
     return [(i, len(i)) for i in lst]
