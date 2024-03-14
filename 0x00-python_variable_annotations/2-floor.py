77af2f8e9b5b76444e39d55c978056203e515544
loaywaleed <loaywaleed9@gmail.com>
2024-03-14 13:42:00 +0200

    returning different types

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script that writes an annotated function called float that returns it floored
+"""
+import typing
+from math import floor
+
+
+def floor(n: float) -> int:
+    """Function that floors a given argument n"""
+    return floor(n)
