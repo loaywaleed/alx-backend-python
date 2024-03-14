23bb27e24110f03a69c6ee998f6ea14185108f19
loaywaleed <loaywaleed9@gmail.com>
2024-03-14 13:53:36 +0200

    returning strings

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Script that writes an annotated function called to_str that returns string representation
+of a float
+"""
+import typing
+
+
+def to_str(n: float) -> str:
+    """Function that floors a given argument n"""
+    return str(n)
