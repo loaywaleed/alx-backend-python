1f193c082ff6a5e84716260a47a35d5f4d0c9a69
loaywaleed <loaywaleed9@gmail.com>
2024-03-14 13:58:38 +0200

    variable annotations

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Script that writes an annotated function called float that returns it floored
+"""
+import typing
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
