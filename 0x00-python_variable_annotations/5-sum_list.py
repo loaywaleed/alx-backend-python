60f4009ef33d7ad07f30f8e5b45664b9d7cf353c
loaywaleed <loaywaleed9@gmail.com>
2024-03-14 14:09:39 +0200

    list of a certain type

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Script that has a function that takes in a list parameter
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """function that adds all elements in a list"""
+    return sum(input_list)
