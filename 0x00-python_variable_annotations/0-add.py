34db4990c4e8cd52f2ec825c97de3d145d3bb861
loaywaleed <loaywaleed9@gmail.com>
2024-03-14 13:22:25 +0200

    pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,9 +2,11 @@
 """
 Script that uses type annotations
 """
-
 import typing
 
-def add(a: float, b: float) -> float:
-    return a + b
 
+def add(a: float, b: float) -> float:
+    """
+    Function that add two values and returns their sum
+    """
+    return a + b
