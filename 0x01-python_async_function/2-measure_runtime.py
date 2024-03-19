aa23caa2223ff4636fab704ab391ce1d069223e5
loaywaleed <loaywaleed9@gmail.com>
2024-03-19 03:17:33 +0200

    runtime duration

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that measures total execution time of aync coroutines
+"""
+import typing
+import asyncio
+import time
+
+wait_n = __import__("1-concurrent_coroutines", wait_n)
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Coroutine that calculates total execution time"""
+    start_time: float = time.perf_counter()
+    asyncio.run((wait_n(n, max_delay)))
+    elapsed_time: float = time.perf_counter() - start_time
+    return (elapsed_timed / n)
+
+
